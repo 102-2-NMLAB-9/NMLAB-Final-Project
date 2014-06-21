f78550194ca90616e2d363277491c139e8fd8c0d
102-2-NMLAB-9 <c1993220888@gmail.com>
2014-06-21 22:32:39 +0800

    direction

--- Assets/Game/Script/clone_ball.js
+++ Assets/Game/Script/clone_ball.js
@@ -4,11 +4,13 @@ var object:GameObject = null;
 
 var grenadeobj : GameObject;
 var mySkin : GUISkin; 
-var showWindow:int = 0;
+var showWindow1:int = 0;
 var state:int = 0;
 var showWindow2:int = 0;
+var showWindow3:int = 0;
 var undelete = ["Terraingreen", "Terrainrock", "Terrainwhite", "Terrainyellow", "Me"];
-var shoot: boolean = false;
+var shoot:boolean = false;
+var yrotaion:int = 0;
 
 function OnGUI()
 {
@@ -16,297 +18,308 @@ function OnGUI()
 	GUILayout.BeginHorizontal();
 	if(GUILayout.Button("create"))
 	{	
-        showWindow++;
+        showWindow1++;
+        showWindow2=0;
+        showWindow3=0;        
 	}
 	if(GUILayout.Button("destroy"))
 	{	
         showWindow2++;
+        showWindow1=0;
+        showWindow3=0;
 	}
-	GUILayout.EndHorizontal();
-	if(showWindow%2==1)
+	GUILayout.EndHorizontal();	
+	GUILayout.Space(5);		
+	if(GUILayout.Button("direction"))
+	{	
+        showWindow3++;
+        showWindow1=0;
+        showWindow2=0; 
+	}	
+	if(showWindow1%2==1)
 	{    
-	   GUI.Window(0,Rect(250,10,1040,610),doWindow,"選單");
+	   GUI.Window(0,Rect(250,10,1040,610),doWindow1,"選單");
 	}
 	if(showWindow2%2==1)
 	{    
 	   GUI.Window(0,Rect(250,10,420,410),doWindow2,"選單");
 	}
+	if(showWindow3%2==1)
+	{    
+	   GUI.Window(0,Rect(250,10,420,155),doWindow3,"選單");
+	}	
 }
 
-function doWindow(windowID:int){
+function doWindow1(windowID:int){
     if(GUI.Button(Rect(10,20,200,60),"spider"))
     {  
-        showWindow++;
+        showWindow1++;
        	state = 0;
         object=GameObject.Find("Spider01");
     }  
     if(GUI.Button(Rect(215,20,200,60),"bench"))
     {  
-        showWindow++;
+        showWindow1++;
         state = 0;
         object=GameObject.Find("Table_Wood");        
         //Debug.Log("spider"); 
     }      
     if(GUI.Button(Rect(420,20,200,60),"black chair"))
     {  
-        showWindow++;
+        showWindow1++;
        	state = 0;
         object=GameObject.Find("plasticchair_white");
     }  
     if(GUI.Button(Rect(625,20,200,60),"shell chair"))
     {  
-        showWindow++;
+        showWindow1++;
         state = 0;
         object=GameObject.Find("Amphi100_Chair02");        
     }     
     if(GUI.Button(Rect(830,20,200,60),"tabouret"))
     {  
-        showWindow++;
+        showWindow1++;
        	state = 0;
         object=GameObject.Find("tabouret_2");
     }          
     if(GUI.Button(Rect(10,85,200,60),"chair"))
     {  
         object=GameObject.Find("Chair_Metal_02");            
-        showWindow++;
+        showWindow1++;
         state = 0;
     }  
     if(GUI.Button(Rect(215,85,200,60),"table"))
     {  
         object=GameObject.Find("Table_Metal");      
-        showWindow++;
+        showWindow1++;
         state = 0;
     }     
     if(GUI.Button(Rect(420,85,200,60),"flower1"))
     {  
-        showWindow++;
+        showWindow1++;
        	state = 0;
         object=GameObject.Find("Furniture_foliageplant_08_LOD1");
     }  
     if(GUI.Button(Rect(625,85,200,60),"flower2"))
     {  
-        showWindow++;
+        showWindow1++;
         state = 0;
         object=GameObject.Find("Furniture_foliageplant_10_LOD0");        
     }     
     if(GUI.Button(Rect(830,85,200,60),"pergola"))
     {  
-        showWindow++;
+        showWindow1++;
        	state = 0;
         object=GameObject.Find("Furniture_flowerstand_01_LOD1");
     }      
     if(GUI.Button(Rect(10,150,200,60),"sofa1"))
     {  
         object=GameObject.Find("sofa1");       
-        showWindow++;
+        showWindow1++;
         state = 0;
     }  
     if(GUI.Button(Rect(215,150,200,60),"sofa2"))
     {  
         object=GameObject.Find("sofa2");       
-        showWindow++;
+        showWindow1++;
         state = 0;
     }  
     if(GUI.Button(Rect(420,150,200,60),"building1"))
     {  
-        showWindow++;
+        showWindow1++;
        	state = 0;
         object=GameObject.Find("Building_10");
     }  
     if(GUI.Button(Rect(625,150,200,60),"building2"))
     {  
-        showWindow++;
+        showWindow1++;
         state = 0;
         object=GameObject.Find("Building_11");        
         //Debug.Log("spider"); 
     }     
     if(GUI.Button(Rect(830,150,200,60),"building3"))
     {  
-        showWindow++;
+        showWindow1++;
        	state = 0;
         object=GameObject.Find("MedievelHouse");
     }      
     if(GUI.Button(Rect(10,215,200,60),"book"))
     {  
     	object = GameObject.Find("book_0001c");
-        showWindow++;
+        showWindow1++;
         state = 0;
     }  
     if(GUI.Button(Rect(215,215,200,60),"gorilla"))
     {  
         object=GameObject.Find("Gorilla");       
-        showWindow++;
+        showWindow1++;
         state = 0;
     }      
     if(GUI.Button(Rect(420,215,200,60),"palm tree"))
     {  
-        showWindow++;
+        showWindow1++;
        	state = 0;
         object=GameObject.Find("Palm");
     }  
     if(GUI.Button(Rect(625,215,200,60),"cactus"))
     {  
-        showWindow++;
+        showWindow1++;
         state = 0;
         object=GameObject.Find("Cactus_Tall_03");        
     }     
     if(GUI.Button(Rect(830,215,200,60),"horse"))
     {  
-        showWindow++;
+        showWindow1++;
        	state = 0;
         object=GameObject.Find("Horse");
     }      
     if(GUI.Button(Rect(10,280,200,60),"pylon"))
     {  
         object=GameObject.Find("pylon_2lanes_lod0");       
-        showWindow++;
+        showWindow1++;
         state = 0;
     }  
     if(GUI.Button(Rect(215,280,200,60),"tower"))
     {  
         object=GameObject.Find("Watchtower_Main");       
-        showWindow++;
+        showWindow1++;
         state = 0;
     }  
     if(GUI.Button(Rect(420,280,200,60),"brick"))
     {  
-        showWindow++;
+        showWindow1++;
        	state = 0;
         object=GameObject.Find("Brick_Red_01");
     }  
     if(GUI.Button(Rect(625,280,200,60),"stone"))
     {  
-        showWindow++;
+        showWindow1++;
         state = 0;
         object=GameObject.Find("stone");        
     }     
     if(GUI.Button(Rect(830,280,200,60),"spider"))
     {  
-        showWindow++;
+        showWindow1++;
        	state = 0;
         object=GameObject.Find("Spider01");
     }      
     if(GUI.Button(Rect(10,345,200,60),"horse"))
     {  
         object=GameObject.Find("Horse");      
-        showWindow++;
+        showWindow1++;
         state = 0;
     }  
     if(GUI.Button(Rect(215,345,200,60),"elephant"))
     {  
-        showWindow++;
+        showWindow1++;
         state = 0;
     }         
     if(GUI.Button(Rect(420,345,200,60),"spider"))
     {  
-        showWindow++;
+        showWindow1++;
        	state = 0;
         object=GameObject.Find("Spider01");
     }  
     if(GUI.Button(Rect(625,345,200,60),"bench"))
     {  
-        showWindow++;
+        showWindow1++;
         state = 0;
         object=GameObject.Find("Table_Wood");        
         //Debug.Log("spider"); 
     }     
     if(GUI.Button(Rect(830,345,200,60),"spider"))
     {  
-        showWindow++;
+        showWindow1++;
        	state = 0;
         object=GameObject.Find("Spider01");
     }       
     if(GUI.Button(Rect(10,410,200,60),"sofa1"))
     {  
         object=GameObject.Find("sofa1");      
-        showWindow++;
+        showWindow1++;
         state = 0;
     }  
     if(GUI.Button(Rect(215,410,200,60),"elephant"))
     {  
-        showWindow++;
+        showWindow1++;
         state = 0;
     }      
     if(GUI.Button(Rect(420,410,200,60),"spider"))
     {  
-        showWindow++;
+        showWindow1++;
        	state = 0;
         object=GameObject.Find("Spider01");
     }  
     if(GUI.Button(Rect(625,410,200,60),"bench"))
     {  
-        showWindow++;
+        showWindow1++;
         state = 0;
         object=GameObject.Find("Table_Wood");        
         //Debug.Log("spider"); 
     }     
     if(GUI.Button(Rect(830,410,200,60),"spider"))
     {  
-        showWindow++;
+        showWindow1++;
        	state = 0;
         object=GameObject.Find("Spider01");
     }        
     if(GUI.Button(Rect(10,475,200,60),"sofa1"))
     {  
         object=GameObject.Find("sofa1");      
-        showWindow++;
+        showWindow1++;
         state = 0;
     }  
     if(GUI.Button(Rect(215,475,200,60),"elephant"))
     {  
-        showWindow++;
+        showWindow1++;
         state = 0;
     }      
     if(GUI.Button(Rect(420,475,200,60),"spider"))
     {  
-        showWindow++;
+        showWindow1++;
        	state = 0;
         object=GameObject.Find("Spider01");
     }  
     if(GUI.Button(Rect(625,475,200,60),"bench"))
     {  
-        showWindow++;
+        showWindow1++;
         state = 0;
         object=GameObject.Find("Table_Wood");        
         //Debug.Log("spider"); 
     }     
     if(GUI.Button(Rect(830,475,200,60),"spider"))
     {  
-        showWindow++;
+        showWindow1++;
        	state = 0;
         object=GameObject.Find("Spider01");
     }      
-    if(GUI.Button(Rect(10,540,200,60),"sofa1"))
-    {  
-        object=GameObject.Find("sofa1");      
-        showWindow++;
+    if(GUI.Button(Rect(10,540,200,60),"normal"))
+    {   
+        showWindow1++;  
         state = 0;
     }  
-    if(GUI.Button(Rect(215,540,200,60),"elephant"))
+    if(GUI.Button(Rect(215,540,200,60),"north"))
     {  
-        showWindow++;
-        state = 0;
+        showWindow1++;   
+        state = 0;               
     }    
-    if(GUI.Button(Rect(420,540,200,60),"spider"))
+    if(GUI.Button(Rect(420,540,200,60),"south"))
     {  
-        showWindow++;
-       	state = 0;
-        object=GameObject.Find("Spider01");
+        showWindow1++;
+       	state = 0;   		
     }  
-    if(GUI.Button(Rect(625,540,200,60),"bench"))
+    if(GUI.Button(Rect(625,540,200,60),"east"))
     {  
-        showWindow++;
-        state = 0;
-        object=GameObject.Find("Table_Wood");        
-        //Debug.Log("spider"); 
+        showWindow1++;
+        state = 0;   
     }     
-    if(GUI.Button(Rect(830,540,200,60),"spider"))
+    if(GUI.Button(Rect(830,540,200,60),"west"))
     {  
-        showWindow++;
-       	state = 0;
-        object=GameObject.Find("Spider01");
+        showWindow1++;   
+       	state = 0;     	     	
     }             
 }  
+
 
 function doWindow2(windowID:int){
 	shoot = false;
@@ -375,6 +388,30 @@ function doWindow2(windowID:int){
     }         
 }  
 
+function doWindow3(windowID:int)
+{
+    if(GUI.Button(Rect(10,20,200,60),"North"))
+    {
+        showWindow3++;
+		yrotaion=0;
+    }
+    if(GUI.Button(Rect(215,20,200,60),"East"))
+    {
+        showWindow3++;
+		yrotaion=90;        
+    }
+    if(GUI.Button(Rect(10,85,200,60),"South"))
+    {
+        showWindow3++;
+		yrotaion=180;        
+    }
+    if(GUI.Button(Rect(215,85,200,60),"West"))
+    {
+        showWindow3++;
+		yrotaion=270;        
+    }            
+}  
+
     function Update() 
 	{
 		if(Input.GetMouseButtonDown(0) && state == 0 && GUIUtility.hotControl == 0)
@@ -385,7 +422,7 @@ function doWindow2(windowID:int){
 		else if(Input.GetMouseButtonDown(0) && state == 1 && GUIUtility.hotControl == 0)
 		{
 			Remove();
-		}
+		}	
 	}
 	
 	function Remove()
@@ -440,8 +477,10 @@ function doWindow2(windowID:int){
 			    objCube.renderer.material.color = Color.magenta;		    
 		    }
 		    else
-		    {		
-		        var clone :GameObject = Instantiate(object, hit.point, object.transform.rotation);
+		    {	
+
+				var myrotation = Quaternion.Euler(object.transform.eulerAngles.x,yrotaion + object.transform.eulerAngles.y,object.transform.eulerAngles.z);
+		        var clone :GameObject = Instantiate(object, hit.point, myrotation);
 		        Destroy (clone, 60);
 		    }
 		}
